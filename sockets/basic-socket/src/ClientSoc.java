919c795ffcc7f79671dd8c52a2d66217d1247a6f
Manish Acharya <contact@manishacharya.name.np>
2025-09-14 15:24:46 +0545

    init

---
+++ sockets/basic-socket/src/ClientSoc.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientSoc {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 9999;
+
+        Scanner scanner = new Scanner(System.in);
+
+        try(Socket socket = new Socket(host, port)) {
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            while (true) {
+                String response = in.readUTF();
+                System.out.println("Server message: " + response);
+
+                System.out.print("Enter a number: ");
+                int val1 = scanner.nextInt();
+                System.out.print("Enter another number: ");
+                int val2 = scanner.nextInt();
+                scanner.nextLine();
+
+                out.writeInt(val1);
+                out.flush();
+                out.writeInt(val2);
+                out.flush();
+
+                int sum = in.readInt();
+                System.out.println("sum is " + sum);
+
+                System.out.print("continue? (y/n): ");
+                String choice = scanner.nextLine();
+                out.writeUTF(choice);
+                out.flush();
+                if (choice.equalsIgnoreCase("n")) {
+                    break;
+                }
+            }
+            socket.close();
+            scanner.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ sockets/basic-socket/src/ServerSoc.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerSoc {
+    private static int sum(int val1, int val2) {
+        return val1 + val2;
+    }
+    public static void main(String[] args) {
+        int port = 9999;
+
+        try(ServerSocket serverSocket = new ServerSocket(port)) {
+            Socket clientSocket = serverSocket.accept();
+
+            DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+
+            while(true) {
+                out.writeUTF("Connected successfully");
+                out.flush();
+
+                int val1 = in.readInt();
+                int val2 = in.readInt();
+                System.out.println("received value: " + val1 + " " + val2);
+
+                int sum = sum(val1,val2);
+                out.writeInt(sum);
+                out.flush();
+
+                String choice = in.readUTF();
+                System.out.println("choice: " + choice);
+                if(choice.equalsIgnoreCase("n")) {
+                    System.out.println("Client Disconnected! Closing Server...");
+                    break;
+                }
+            }
+
+            clientSocket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
