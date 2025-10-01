c5ade08f5df908d8c9f52a2f6846163e91ad8491
Manish Acharya <manuacharya54@gmail.com>
2025-10-01 09:52:37 +0545

    simple testing of server on rust forwarding html response(also parse content-type)

--- chat-rs/src/main.rs
+++ chat-rs/src/main.rs
@@ -7,12 +7,32 @@ use std::{
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     if let Ok(_) = stream.read(&mut buffer) {
-        let html = fs::read_to_string("public/index.html")
-            .unwrap_or_else(|_| "<h1>File not Found :P</h1>".to_string());
+        let request = String::from_utf8_lossy(&buffer);
+
+        let path = request
+            .lines()
+            .next()
+            .and_then(|line| line.split_whitespace().nth(1))
+            .unwrap_or("/");
+
+        let path = if path == "/" { "index.html" } else { path };
+        let filepath = format!("public/{}", path);
+        let body = fs::read_to_string(&filepath)
+            .unwrap_or_else(|_| "<h1>404 - File not Found :P</h1>".to_string());
+
+        let content_type = if filepath.ends_with(".html") {
+            "text/html"
+        } else if filepath.ends_with(".css") {
+            "text/css"
+        } else if filepath.ends_with(".js") {
+            "application/javascript"
+        } else {
+            "text/plain"
+        };
 
         let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset\r\n\r\n{}",
-            html
+            "HTTP/1.1 200 OK\r\nContent-Type: {}; charset\r\n\r\n{}",
+            content_type, body
         );
 
         stream.write_all(response.as_bytes()).unwrap();
